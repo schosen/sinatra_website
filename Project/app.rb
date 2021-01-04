427c66565a49fe6dd76c01b00e3ee663330bfbb9
Sarah <sarahchosen@hotmail.co.uk>
2021-01-04 16:17:32 +0000

    added image

--- Project/app.rb
+++ Project/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   'hello!'
 end
 
 get '/secret' do
-  'This is a secret page'
+  'This is a VERRRYYYY secret page'
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
