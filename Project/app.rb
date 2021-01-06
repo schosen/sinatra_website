3a0c3930b905e00be893b7298e90def2b2d94097
Sarah <sarahchosen@hotmail.co.uk>
2021-01-06 15:00:14 +0000

    added param to named cat page

--- Project/app.rb
+++ Project/app.rb
@@ -10,7 +10,14 @@ get '/secret' do
   'This is a VERRRYYYY secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample 
+  erb(:index)
+end
+
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
